aaa0cc4e67775c09800f9eb7cb19e2912549d640
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-28 22:15:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,8 +23,7 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheese)
-  cheese.find do |x|
-    x == "cheese"
+cheese.shift
   end
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
