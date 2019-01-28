3e8d5c10a2e3a2875e0612c25a1c10aedb8affd6
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-28 21:41:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  puts dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(fruits)
+  fruits.capitalize
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |x|
+    x.length > 4
 end
 
 def find_the_cheese# code an argument here
